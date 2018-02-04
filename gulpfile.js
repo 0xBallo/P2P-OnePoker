45535d43c533dcf836595191a48bd3e362f6bda4
mballo <mattia.ballo@outlook.com>
2018-02-04 14:53:33 +0100

    add static web server for testing

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
+var connect = require('gulp-connect');
+var open = require('gulp-open');
 
 // Compile Sass into CSS & auto-inject into browser
 gulp.task('sass', function () {
@@ -13,8 +15,9 @@ gulp.task('sass', function () {
 // Move Js files to /src/js folder
 gulp.task('js', function () {
   return gulp.src([
-    'node_modules/deck-of-cards/dist/deck.min.js', 'node_modules/pokersolver/pokersolver.js', 
-    'node_modules/peerjs/dist/peer.min.js'])
+      'node_modules/deck-of-cards/dist/deck.min.js', 'node_modules/pokersolver/pokersolver.js',
+      'node_modules/peerjs/dist/peer.min.js'
+    ])
     .pipe(gulp.dest('src/js'))
     .pipe(browserSync.stream());
 });
@@ -36,5 +39,28 @@ gulp.task('serve', ['sass'], function () {
   gulp.watch(['src/scss/*.scss'], ['sass']);
   gulp.watch(['src/*.html', 'src/js/*.js', 'src/css/*.css']).on('change', browserSync.reload);
 })
+
+// Web server for testing
+gulp.task('webserver', function () {
+  connect.server({
+    port: 8887,
+    root: 'src',
+    host: 'localhost'
+  });
+});
+
+// Open browser window
+gulp.task('test', ['webserver'], function () {
+  var options = {
+    uri: 'http://localhost:8887'
+  };
+
+  for (var i = 0; i < 4; i++) {
+    gulp.src('')
+      .pipe(open(options));
+  }
+
+});
+
 
 gulp.task('default', ['js', 'CSS', 'serve']);
